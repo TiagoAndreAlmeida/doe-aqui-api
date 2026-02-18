024c4a0fce601dcb21c45d9516a3350aaf7f59b8
Tiago Almeida <thyagoeap@gmail.com>
2026-02-18 17:09:58 -0300

    commit inicial de criação do projeto.

---
+++ src/test/java/br/com/doeaqui/DoeAquiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doeaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeAquiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
